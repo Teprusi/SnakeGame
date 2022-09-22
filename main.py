d707e02a1a4de8848b611d6abc443b8296e5866e
rpilinci <ronaldopilinci22@gmail.com>
2022-09-22 19:03:20 +0200

    Update

--- main.py
+++ main.py
@@ -4,45 +4,45 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
-head = snake.segments[0]
-
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.tracer(0)
-screen.title("Ron Snake Game")
 
 screen.listen()
-screen.onkey(fun=snake.up, key="Up")
-screen.onkey(fun=snake.down, key="Down")
-screen.onkey(fun=snake.left, key="Left")
-screen.onkey(fun=snake.right, key="Right")
-
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.07)
+    time.sleep(0.05)
     snake.move()
 
-    # Detect collision with food
-    if snake.segments[0].distance(food) < 15:
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.score_up()
+        scoreboard.increase_score()
 
     # Detect collision with wall.
-    if head.xcor() > 280 or head.xcor() < -280 or head.ycor() > 280 or head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
 
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    # Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,24 +1,31 @@
 from turtle import Turtle
-ALIGNMENT = 'center'
-FONT = ('Courier', 10, 'normal')
+ALIGNMENT = "center"
+FONT = ("Courier", 12, "normal")
 
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.penup()
         self.score = 0
-        self.color('white')
+        self.high_score = 0
+        self.color("white")
+        self.penup()
         self.goto(0, 270)
-        self.write(f'Score: {self.score}', move=False, align=ALIGNMENT, font=FONT)
         self.hideturtle()
+        self.update_scoreboard()
 
-    def score_up(self):
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f'Score: {self.score}', move=False, align=ALIGNMENT, font=FONT)
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write('GAME OVER', align=ALIGNMENT, font=('Courier', 20, 'normal'))
+        self.update_scoreboard()
